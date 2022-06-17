1b8e2da4003b887c7ed2e55276cf08c8e841ab73
JJangGyu <pridom112@naver.com>
2022-06-17 16:26:39 +0900

    first commit

---
+++ vue-news/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    Hello
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    fethData(){
+      console.log('hello');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
