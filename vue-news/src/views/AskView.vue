db2e2c8ea2ba850758904685910d56516f85c6aa
JJangGyu <pridom112@naver.com>
2022-06-21 15:20:56 +0900

    Ask/JobsView Styling

--- vue-news/src/views/AskView.vue
+++ vue-news/src/views/AskView.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    <p v-for="ask in askItems" v-bind:key="ask.id">
-      <router-link v-bind:to="`item/${ask.id}`">
-        {{ ask.title }}
-      </router-link>
-      <small>{{ ask.time_ago }} by {{ask.user}}</small>
-    </p>
+    <ul class="news-list">
+      <li v-for="ask in askItems" v-bind:key="ask.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ ask.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <router-link v-bind:to="`item/${ask.id}`">
+              {{ ask.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            by
+            <router-link v-bind:to="`/user/${ask.user}`" class="link-text">
+              {{ ask.user }}
+            </router-link>  
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -33,6 +49,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.news-list{
+  margin: 0;
+  padding: 0;
+}
+.post{
+  list-style : none;
+  display: flex;
+  align-items: center;
+  border-bottom : 1px solid #eee;
+}
+.points{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 60px;
+  color: #42b883;
+}
+.news-title{
+  margin: 0;
+}
+.link-text{
+  color: #828282;
+}
 </style>
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,23 +1,67 @@
 <template>
   <div>
+    <!-- 
     <p v-for="job in this.$store.state.jobs" v-bind:key="job.id">
-      <!-- :href 축약됨 > v-bind:href랑 같음 -->
       <a :href="job.url">{{ job.title }}</a>
       <small>{{ job.time_ago }} by {{job.domain}}</small>
-    </p>
+    </p> 
+    -->
+
+    <ul class="news-list">
+      <li v-for="job in this.$store.state.jobs" v-bind:key="job.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ job.point || 0 }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <!-- :href 축약됨 > v-bind:href랑 같음 -->
+            <a :href="job.url">{{ job.title }}</a>
+          </p>
+          <small class="link-text">
+            by
+            <a :href="job.url" class="link-text">
+              {{job.domain}}
+            </a>
+          </small>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-
-
 export default {  
   created() {
     this.$store.dispatch('FETCH_JOBS');
   }
 }
 </script>
-
-<style>
+<style scoped>
+.news-list{
+  margin: 0;
+  padding: 0;
+}
+.post{
+  list-style : none;
+  display: flex;
+  align-items: center;
+  border-bottom : 1px solid #eee;
+}
+.points{
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  width: 80px;
+  height: 60px;
+  color: #42b883;
+}
+.news-title{
+  margin: 0;
+}
+.link-text{
+  color: #828282;
+}
 
 </style>
