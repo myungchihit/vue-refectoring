f9e3a0f35cee5746b76c7b16f247251a237ecf66
JJangGyu <pridom112@naver.com>
2022-06-24 19:41:08 +0900

    chart.js practice

---
+++ vue-chart/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Chanrt.js</h2>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
+
+// export default는 컴포넌트 속성과 인스턴스 옵션 선언하는곳.
+// mounted하고 script 작성
+export default {
+  components: {
+    BarChart,
+    LineChart
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-chart/src/components/LineChart.vue
@@ -0,0 +1,46 @@
+<template>
+  <canvas id="lineChart"></canvas>
+</template>
+
+<script>
+import Chart from 'chart.js';
+
+export default {
+    mounted() {
+        const labels = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+        ];
+
+        const data = {
+            labels: labels,
+            datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        };
+
+        const config = {
+            type: 'line',
+            data: data,
+            options: {}
+        };
+
+        // eslint-disable-next-line no-unused-vars
+        const myChart = new Chart(
+        document.getElementById('lineChart'),
+            config,
+        );
+    }
+}
+</script>
+
+<style>
+
+</style>
